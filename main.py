6edc9eacc0bcd9621a6f43d68fadcf23969cb3a4
Adam Wiśniewski <norr.pl@gmail.com>
2024-02-27 16:59:03 +0100

    config

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("..")
 import starlette.status as status
-
+import os
 from fastapi import FastAPI, responses
 from routers import auth, translate, game, home
 from starlette.staticfiles import StaticFiles
@@ -25,4 +25,6 @@ async def redirect():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True,
+                ssl_keyfile=os.getenv('PRIVATE_CERT'),
+                ssl_certfile=os.getenv('PUBLIC_CERT'))
